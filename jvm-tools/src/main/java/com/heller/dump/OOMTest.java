8ddaefe3308b415563770c27cacf5a654624c928
heller <heller@heller.com>
2020-08-12 21:35:38 +0800

    测试 OOM，分析 GC 日志和 dump 文件

--- jvm-tools/src/main/java/com/heller/dump/OOMTest.java
+++ jvm-tools/src/main/java/com/heller/dump/OOMTest.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
-    运行环境 java 8
-    Java 运行参数：
+    运行环境 java 8                                                 <br/>
+    Java 运行参数：                                                  <br/>
 
     -Xms10m -Xmx10m
-    -XX:+PrintGCDetails                         输出详细GC日志
-    -Xloggc:/Users/heller/tmp/oom_gc.log        输出GC日志到文件
-    -XX:+PrintGCDateStamps                      输出GC的时间戳（以日期的形式，如 2013-05-04T21:53:59.234+0800）
-    -XX:+PrintHeapAtGC                          在进行GC的前后打印出堆的信息
-    -XX:+PrintReferenceGC                       打印年轻代各个引用的数量以及时长
-    -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/Users/heller/tmp/oom.dump
+    -XX:+PrintGCDetails                         输出详细GC日志        <br/>
+    -Xloggc:/Users/heller/tmp/oom_gc.log        输出GC日志到文件       <br/>
+    -XX:+PrintGCDateStamps                      输出GC的时间戳（以日期的形式，如 2013-05-04T21:53:59.234+0800） <br/>
+    -XX:+PrintHeapAtGC                          在进行GC的前后打印出堆的信息     <br/>
+    -XX:+PrintReferenceGC                       打印年轻代各个引用的数量以及时长        <br/>
+    -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/Users/heller/tmp/oom.dump     <br/>
  */
 
 public class OOMTest {
